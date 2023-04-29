600b7dc77b6eab12b175a8b16ce23d4600c8d7eb
Daniel Kert <danielkertdev@gmail.com>
2023-04-29 11:04:29 +0300

    commit - 29 april kodar med madde

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require('express'); // import express
+const app = express(); // create express app
+const cors = require('cors'); // import cors
+const path = require('path'); // import path
+
+app.use(express.json()) // Middleware parse
+app.use(cors()) // Middleware cors
+
+//? User
+
+//? Quiz
+
+//? Admin
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.APP_PORT}`);
+})
