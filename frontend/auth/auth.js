775e78ed103d2c76bb6110d63720f93150a209b0
Daniel Kert <danielkertdev@gmail.com>
2023-05-08 01:35:53 +0300

    commit may 8 morning

--- frontend/auth/auth.js
+++ frontend/auth/auth.js
@@ -1,13 +1,43 @@
-function login() {
-    alert("You are logged in!");
+function authPage() {
+  document.querySelector("main").innerHTML = `
+      <div class="login-signup-container">
+        <div class="login">
+          <h2>Login</h2>
+          <input type="text" placeholder="Username" class="login-username">
+          <input type="password" placeholder="Password" class="login-password">
+          <button class="login-button">Log-in</button>
+        </div>
+  
+        <div class="signup">
+          <h2>Signup</h2>
+          <input type="text" placeholder="Username" class="signup-username">
+          <input type="password" placeholder="Password" class="signup-password">
+          <input type="email" placeholder="Email" class="signup-email">
+          <button class="signup-button">Sign-up</button>
+        </div>
+      </div>
+    `;
+
+  document.querySelector(".login-button").addEventListener("click", () => {
+    const loginUsername = document.querySelector(".login-username").value;
+    const loginPassword = document.querySelector(".login-password").value;
+    login(loginUsername, loginPassword);
+  });
+
+  document.querySelector(".signup-button").addEventListener("click", () => {
+    const signupUsername = document.querySelector(".signup-username").value;
+    const signupPassword = document.querySelector(".signup-password").value;
+    const signupEmail = document.querySelector(".signup-email").value;
+    signup(signupUsername, signupPassword, signupEmail);
+  });
 }
 
-function signup() {
-    alert("You are signed up!");
+function login(username, password) {
+  alert("Welcome " + username);
 }
 
-function logout() {
-    alert("You are logged out!");
+function signup(username, password, email) {
+  alert("Welcome " + username);
 }
 
-export { login, signup, logout };
+export { authPage };
--- frontend/index.js
+++ frontend/index.js
@@ -1,18 +1,37 @@
-import {login, signup, logout} from "./auth/auth.js";
+import { authPage,
+} from "./auth/auth.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     header();
     footer();
+    titleClick();
 })
 
 function header() {
     document.querySelector("header").innerHTML = `
-    <h1>GUESS RIGHT</h1>
-    `
+    <h1 class="title">GUESS RIGHT</h1>
+
+    <div class="header-objects">
+
+    <div class="user-info"></div>
+
+        <button class="login-signup"'>Log-in / Sign-up</button>
+    </div>
+
+    </div>
+    `;
+
+    document.querySelector(".login-signup").addEventListener("click", authPage);
 }
 
 function footer() {
     document.querySelector("footer").innerHTML = `
     © ${new Date().getFullYear()} - Daniel Kertsmik
     `    
+}
+
+function titleClick() {
+    document.querySelector(".title").addEventListener("click", () => {
+        document.querySelector("main").innerHTML = ``
+    })
 }
