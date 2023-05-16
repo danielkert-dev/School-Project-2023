0e63957f3f32b97c8ed7627661e2cb805510df30
Daniel Kert <danielkertdev@gmail.com>
2023-05-16 20:00:46 +0300

    commit may 16 20:20
    Plans for tomorrow and some fixes

--- frontend/create/quiz.create.js
+++ frontend/create/quiz.create.js
@@ -3,13 +3,13 @@ import { mainPage, transition } from "../index.js";
 // quiz/Create
 
 function createPage() {
-    document.querySelector("header").style.display = "none";
-    document.querySelector("footer").style.display = "none";
-    document.querySelector(".search").innerHTML = ``;
+  document.querySelector("header").style.display = "none";
+  document.querySelector("footer").style.display = "none";
+  document.querySelector(".search").innerHTML = ``;
 
-    localStorage.setItem("createQuestion", 0);
-    
-    document.querySelector("main").innerHTML = `
+  localStorage.setItem("createQuestion", 0);
+
+  document.querySelector("main").innerHTML = `
     <div class="create-quiz-container">
 
     <div class="create-header">
@@ -22,6 +22,7 @@ function createPage() {
     <div class="create-body">
 
     <div class="create-form-quiz">
+    <div class="create-alert"></div>
     <button class="create-button">Create Quiz</button>
 
     <h3>Quiz Title <p class="create-comment">Title of the quiz</p></h3>
@@ -33,8 +34,8 @@ function createPage() {
 
     <button class="add-question-button">Add Question</button>
     </div>
+
     <div class="create-form-question">
-    
     </div>
 
     </div>
@@ -42,97 +43,118 @@ function createPage() {
   `;
 
   document.querySelector(".add-question-button").addEventListener("click", () => {
-      questionAdd();
-  })
+      questionBoxAdd();
+    });
 
   document.querySelector(".create-button").addEventListener("click", () => {
-      quizAdd();
-  })
+    quizAdd();
+  });
 
   document.querySelector(".back-button").addEventListener("click", () => {
     transition();
     setTimeout(() => {
-        document.querySelector("header").style.display = "flex";
-        document.querySelector("footer").style.display = "flex";
-        mainPage();
+      document.querySelector("header").style.display = "flex";
+      document.querySelector("footer").style.display = "flex";
+      mainPage();
     }, 100);
-  })
-
+  });
 }
 
-function questionAdd () {
-    localStorage.setItem("createQuestion", parseInt(localStorage.getItem("createQuestion")) + 1);
-    document.querySelector(".create-form-question").innerHTML += `
-    <div class="create-question-box" id="create-question-${localStorage.getItem("createQuestion")}">
+function questionBoxAdd() {
+  localStorage.setItem(
+    "createQuestion",
+    parseInt(localStorage.getItem("createQuestion")) + 1
+  );
+  document.querySelector(".create-form-question").innerHTML += `
+    <div class="create-question-box" id="create-question-${localStorage.getItem(
+      "createQuestion"
+    )}">
     <h3>Question ${localStorage.getItem("createQuestion")}</h3>
     </div>
-    `
+    `;
 }
-
 
 function quizAdd() {
 
-    // Fetch user/SearchByUsername/username
-    fetch(`${window.API}/user/searchByUsername/${localStorage.getItem("username")}`, {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
-    })
-    .then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-        throw new Error(response.statusText);
-        }
-    })
-    .then(data => {
-        console.log(data);
-
+    
     let quizTitle = document.querySelector(".create-form-quiz-title").value;
-    let quizDescription = document.querySelector(".create-form-quiz-description").value;
-    let userID = data.data[0].ID
+    let quizDescription = document.querySelector(
+      ".create-form-quiz-description"
+    ).value;
     let quizImage = document.querySelector(".create-form-quiz-image").value;
 
-    console.log(quizTitle, quizDescription, userID, quizImage);
+    if (localStorage.getItem("createQuestion") < 3) {
+        return document.querySelector(".create-alert").innerHTML = `
+        <p class="create-error"> Please add at least 2 questions </p>
+        `;
+    }
 
-    // Fetch quiz/Create body: title,description, user_ID, image
-    fetch(`${window.API}/quiz/Create`, {
+  // Fetch user/SearchByUsername/username
+  fetch(
+    `${window.API}/user/searchByUsername/${localStorage.getItem("username")}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.statusText);
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      let userID = data.data[0].ID;
+
+      console.log(quizTitle, quizDescription, userID, quizImage);
+
+      // Fetch quiz/Create body: title,description, user_ID, image
+      fetch(`${window.API}/quiz/Create`, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
         body: JSON.stringify({
-            title: quizTitle,
-            description: quizDescription,
-            user_ID: userID,
-            image: quizImage
-        })
-    }).then((response) => {
-        if (response.ok) {
+          title: quizTitle,
+          description: quizDescription,
+          user_ID: userID,
+          image: quizImage,
+        }),
+      })
+        .then((response) => {
+          if (response.ok) {
             return response.json();
-        } else {
-        throw new Error(response.statusText);
-        }
-    }).then(data => {
-        console.log("Created Quiz", data);
-        transition();
-        setTimeout(() => {
+          } else {
+            throw new Error(response.statusText);
+          }
+        })
+        .then((data) => {
+          console.log("Created Quiz", data);
+          transition();
+          setTimeout(() => {
             createPage();
-        },100)
-    }).catch((error) => {
-        document.querySelector("header").style.display = "flex";
-        document.querySelector("footer").style.display = "flex";
-        mainPage();
+          }, 100);
+        })
+        .catch((error) => {
+          document.querySelector("header").style.display = "flex";
+          document.querySelector("footer").style.display = "flex";
+          mainPage();
+        });
     })
-
-
-}).catch((error) => {
-    document.querySelector("header").style.display = "flex";
-    document.querySelector("footer").style.display = "flex";
-    mainPage();
-})
+    .catch((error) => {
+      document.querySelector("header").style.display = "flex";
+      document.querySelector("footer").style.display = "flex";
+      mainPage();
+    });
 }
 
-export { createPage }
+function questionAdd() {
+  // Fetch quiz/QuestionAdd and make a loop that takes from localStorage
+}
+
+export { createPage };
